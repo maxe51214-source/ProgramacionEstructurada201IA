49ef0361384cd9d101a94a24dd7ff2495c1f0c55
max <maxe51214@gmail.com>
2026-03-13 08:25:14 -0600

    ejercicio de cadenas

---
+++ manejo_cadena/ejercicio_cadenas.py
@@ -0,0 +1,14 @@
+# ejercicio de cadenas
+
+def demostracion_cadenas ():
+    cadena = "Universidad de Xalapa"
+    print(cadena.upper())
+    print(cadena. lower())
+    print(cadena.capitalize())
+    print(cadena.title())
+
+def main():
+    demostracion_cadenas ()
+
+if __name__ == "main ":
+    main()
---
+++ src/ux.com.curso,programacion/unidad1/tipos_de_datos.py
@@ -0,0 +1,26 @@
+def datos():
+    entero = 25
+    decimal = 3.14
+    cadena = "Hola mundo!"
+    booleano = True
+
+    print(entero)
+    print(decimal)
+    print(cadena)
+    print(booleano)
+
+def tipos_datos_compuestos():
+    lista = [10, 20, 30, 40]
+    tupla = (19, 29, 39, 49)
+    diccionario = {"Nombre": "Maximiliano", "Edad": 18, "Ciudad": "Xalapa"}
+
+    print(lista)
+    print(tupla)
+    print(diccionario)
+
+def main():
+    datos()
+    tipos_datos_compuestos()
+
+if __name__ == "__main__":
+    main()
