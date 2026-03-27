f2de4698f8ff1f7a25fff2e8a35190a6386eea5f
max <maxe51214@gmail.com>
2026-03-27 08:40:40 -0600

    ejercicio01

--- src/ux.com.curso,programacion/unidad2/mermei/ejercicio01.py
+++ src/ux.com.curso,programacion/unidad2/mermei/ejercicio01.py
@@ -2,3 +2,17 @@
 diseñar un algoritmo que calcule la suma de todos lus numeros enteros del 1 al 100
 que son divisibles por 3, y además, impares
 """
+def es_divisible():
+    while i<=100:
+        if i % 3==0 and i%2!=0:
+            suma += i
+            i += 1
+            return suma
+     
+
+def main():
+    suma = es_divisible()
+    print(suma)
+
+if __name__=="__main__":
+    main()
